2820613f26dc71bd01b6a6eb769ee017dcb71773
Sarb <93352656+Sarbkum@users.noreply.github.com>
2021-12-12 13:16:24 +0000

    Add files via upload

---
+++ Task 3 Week 1.py
@@ -0,0 +1,8 @@
+
+starter = input("Please enter your favourite starter:")
+main = input("Please enter your main course:")
+dessert = input("Please enter your desset:")
+drink = input("Please enter your drink:")
+
+print('Your favourite meal is '+ starter, main, dessert + ' with a glass of ' + drink)
+
